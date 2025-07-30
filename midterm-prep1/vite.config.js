b08feab03224dc6b265717f5d1b1389e7533d390
Wai Yan <waiyanpaing183@gmail.com>
2025-07-31 00:31:41 +0700

    Fix vite base path for midterm-prep1

--- midterm-prep1/vite.config.js
+++ midterm-prep1/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/waiyanpaing.github.io/midterm-prep1-build/',
   plugins: [react()],
 })
