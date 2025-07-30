fe536ce14f87496b42a2bf1fdcccafc694bc6798
Wai Yan <waiyanpaing183@gmail.com>
2025-07-31 00:18:31 +0700

    Fix broken links by updating build

--- midterm-prep/vite.config.js
+++ midterm-prep/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/midterm-prep-build/', 
+  base: '/waiyanpaing.github.io/midterm-prep-build/', 
   plugins: [react()],
 })
