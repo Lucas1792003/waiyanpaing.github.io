618d0a58fd8938db4f59eac2e593c29a88b45353
Wai Yan <waiyanpaing183@gmail.com>
2025-08-14 17:52:30 +0700

    calculator path fix

--- react_calculator/vite.config.js
+++ react_calculator/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/waiyanpaing.github.io/react-calculator-build/', 
+  base: '/waiyanpaing.github.io/react_calculator-build/', 
   plugins: [react()],
 })
